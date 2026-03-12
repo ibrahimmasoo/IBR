afe869c89804f91bc01ef51e71f3d232aca2df0a
ibrahimmasoo <114830592+ibrahimmasoo@users.noreply.github.com>
2026-03-12 06:58:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const observer = new IntersectionObserver(entries=>{
+entries.forEach(entry=>{
+if(entry.isIntersecting){
+entry.target.classList.add("show");
+}
+});
+});
+
+document.querySelectorAll(".card").forEach(el=>observer.observe(el));
