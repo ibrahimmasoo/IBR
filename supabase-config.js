d02c83ad1782f72019bae5b7d32d77e64694b8c9
ibrahimmasoo <114830592+ibrahimmasoo@users.noreply.github.com>
2026-03-22 15:51:21 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,3 @@
-window.SUPABASE_URL = "https://tgbgeobkdolfxkzmtxf.supabase.co";
+window.SUPABASE_URL = "https://tgbgeobkdolflxkzmtxf.supabase.co";
+
 window.SUPABASE_ANON_KEY = "sb_publishable_CIB-5vcpEdXNpAz7I4zgtA_z7PDB5iU";
